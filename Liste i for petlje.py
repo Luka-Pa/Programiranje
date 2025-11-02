d2915e3cc836b72e2b5a3841311902cedf69f02f
Luka-Pa <luka2009stantonio@gmail.com>
2025-11-02 18:15:54 +0100

    Add files via upload

---
+++ Liste i for petlje.py
@@ -0,0 +1,12 @@
+#Kreiramo praznu listu
+otpornici = []
+#Dodajemo otpornike u listu
+otpornici.append(100)
+otpornici.append(220)
+otpornici.append(330)
+#Provjeri opet što je u listi:
+print(otpornici)  # Rezultat: [100, 220, 330]
+#Provjeri koliko stvari ima u listi:
+print(len(otpornici))  # Rezultat: 3
+#Dohvati prvu stvar (indeksiranje kreće od 0):
+print(otpornici[0])  # Rezultat: 100
